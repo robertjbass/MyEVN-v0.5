9cc8c7fc54a7a53b2f060017b5283e904377d26e
Bob Bass <bob@bbass.co>
2020-03-31 21:27:11 -0400

    fixed formatting

--- client/src/components/Debtor.vue
+++ client/src/components/Debtor.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="debtorCardclass" v-bind="debtors">
-    <div>
-      <div class="debtorCard" v-for="debtor in debtors" :key="debtor.id">
-        <div class="left">
-          <div class="handle" :style="`background-color: ${debtor.color} `">
-            <br />⋮<br /><br />
-          </div>
-          <br />
+  <div class="debtors" v-bind="debtors">
+    <div class="debtor-card" v-for="debtor in debtors" :key="debtor.id">
+      <div class="handle" :style="`background-color: ${debtor.color} `"></div>
+      <div class="card-text">
+        <div class="left-text">
           Debtor ID: {{ debtor.id }}<br />
           File Number: {{ debtor.filenumber }}<br />
           State: {{ debtor.state }}
         </div>
-        <div class="right">
-          <br />
+        <div class="right-text">
           {{ debtor.name }}<br />
           {{ debtor.address }} {{ debtor.address2 }}<br />
           {{ debtor.city }}, {{ debtor.state }} {{ debtor.zip }}
@@ -32,6 +28,12 @@ export default {
       return debtors.debtors.map(debtors => {
         return debtors;
       });
+    },
+    // Not operational yet. Working on dynamic colors
+    debtorStatus() {
+      return debtors.debtors.map(debtors => {
+        return debtors;
+      });
     }
   }
 };
@@ -46,51 +48,55 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-.debtorCard {
+.debtor-card {
+  display: flex;
+  flex-wrap: nowrap;
   border-style: solid;
   border-width: 1px;
-  border-radius: 1em;
+  border-radius: 1rem;
   margin: 5px 10px 5px 10px;
   padding: 0% 0% 0% 0%;
   border: 2px;
   background-color: #2d3436;
   color: #b2bec3;
-  flex-grow: 1;
-  display: flex;
-  border-style: solid;
   height: 100%;
+  max-width: 100%;
+  flex-grow: 1;
 }
 .cardLink,
 a {
   text-decoration: none;
 }
-.debtorCard:hover {
+.debtor-card:hover {
   background: #a29bfe7c;
 }
-.debtorCardClass {
-  display: flex;
-}
 .handle {
-  height: 100%;
-  width: 15px;
-  text-align: center;
-  font-size: 1.5rem;
-  margin: 0px 10px 0px 0px;
-  border-radius: 70em 4em 4em 70em;
-  color: black;
-  /* Acts as a backup in the event of a color render failure */
-  background-color: white;
-  align-self: center;
+  max-height: 100%;
+  width: 3%;
+  min-width: 15px;
+  border-radius: 3em 3em 3em 3em;
+  margin: 10px 1px 10px 1px;
 }
 .handle:hover {
   cursor: pointer;
 }
-.left {
+.left-text {
+  padding: 5px;
+  min-width: 45%;
   display: flex;
-  min-width: 50%;
-  text-align: left;
+  flex-wrap: nowrap;
 }
-.right {
-  min-width: 50%;
+.right-text {
+  padding: 5px;
+  min-width: 45%;
+  display: flex;
+  flex-wrap: nowrap;
+}
+.card-text {
+  display: flex;
+  padding: 10px;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  flex-grow: 1;
 }
 </style>
